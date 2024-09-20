24a88d990f1679a3f3a817be7d305c2de1ce30a2
halmasi <h.almasi2012@gmail.com>
2024-09-20 15:56:09 +0330

    edit some styles and add new photo as personal image

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -3,42 +3,47 @@ import Link from "next/link";
 import { BiDetail } from "react-icons/bi";
 
 interface Props {
-  id?: number;
-  title: string;
-  publishDate: string;
-  describtion: string;
-  picture: string;
+	id?: number;
+	title: string;
+	publishDate: string;
+	describtion: string;
+	picture: string;
 }
 
 export default function BlogCard({
-  id,
-  title,
-  describtion,
-  picture,
-  publishDate,
+	id,
+	title,
+	describtion,
+	picture,
+	publishDate,
 }: Props) {
-  return (
-    <div className="flex flex-col space-y-3 p-4 rounded-xl border-2 w-full">
-      <div className="flex flex-row justify-between text-xs text-gray-500">
-        <p>published at: {publishDate}</p>
-        <p></p>
-      </div>
-      <Image
-        src={picture}
-        alt={title}
-        width={500}
-        height={0}
-        className="w-full object-cover rounded-md max-h-52"
-      />
-      <h2 className="text-2xl font-bold drop-shadow-sm">{title}</h2>
-      <p>{describtion}</p>
-      <Link
-        href={"#"}
-        className="flex flex-row px-4 py-1 rounded-full border mb-3 hover:bg-red-50/50 transition-colors duration-300 w-36 items-center border-red-900"
-      >
-        <BiDetail className="mr-2 text-lg" />
-        <p className="text-sm">read more...</p>
-      </Link>
-    </div>
-  );
+	return (
+		<div className='flex flex-col space-y-3 p-4 rounded-xl border-2 w-full'>
+			<div className='flex flex-row justify-between text-xs text-gray-500'>
+				<p>
+					category:{" "}
+					<Link className='text-red-900' href={"#"}>
+						category
+					</Link>
+				</p>
+				<p>publish date: {publishDate}</p>
+			</div>
+			<Image
+				src={picture}
+				alt={title}
+				width={500}
+				height={0}
+				className='w-full object-cover rounded-md max-h-52'
+			/>
+			<h2 className='text-2xl font-bold drop-shadow-sm'>{title}</h2>
+			<p>{describtion}</p>
+			<Link
+				href={"#"}
+				className='flex flex-row px-4 py-1 rounded-full border mb-3 hover:bg-red-50/50 transition-colors duration-300 w-36 items-center border-red-900'
+			>
+				<BiDetail className='mr-2 text-lg' />
+				<p className='text-sm'>read more...</p>
+			</Link>
+		</div>
+	);
 }
--- app/components/homePage/SkillCard.tsx
+++ app/components/homePage/SkillCard.tsx
@@ -2,40 +2,40 @@ import Image from "next/image";
 import { levelCalc, iconNameGenerator } from "../utils";
 
 interface Props {
-  title: string;
-  level: number;
+	title: string;
+	level: number;
 }
 
 export default function Skill({ title, level }: Props) {
-  const { levels, progressBarPercent } = levelCalc(level);
+	const { levels, progressBarPercent } = levelCalc(level);
 
-  return (
-    <div className="relative max-w-xl flex flex-row flex-grow border-2 rounded-lg w-full min-h-36 md:aspect-square justify-between bg-neutral-800">
-      <Image
-        className=" w-full object-cover rounded-md"
-        width={500}
-        height={0}
-        quality={50}
-        src={`/icons/${iconNameGenerator(title)}`}
-        alt={title + "Logo"}
-      />
-      <div className="absolute inset-0 flex flex-col items-center before:absolute before:inset-0 before:bg-slate-400 before:transition-opacity before:duration-300 before:hover:opacity-20 before:opacity-40 before:rounded-md">
-        <h2 className="text-white text-2xl font-bold">{title}</h2>
-        <div className=" flex flex-col justify-end w-full h-full">
-          <div className="w-full flex flex-col justify-end">
-            <p className="text-gray-800 text-center leading-tight pb-5 bg-white bg-opacity-50">
-              <strong>{levels}</strong>
-            </p>
-            <div className="w-full bg-gray-400 h-2.5 rounded-b-md">
-              <div
-                className={`${progressBarPercent} bg-red-700 h-2.5 ${
-                  level == 100 ? "rounded-b-md" : "rounded-bl-md"
-                } `}
-              ></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='relative max-w-xl flex flex-row flex-grow border-2 rounded-lg w-full min-h-36 md:aspect-square justify-between bg-neutral-800'>
+			<Image
+				className=' w-full object-cover rounded-md'
+				width={500}
+				height={0}
+				quality={50}
+				src={`/icons/${iconNameGenerator(title)}`}
+				alt={title + "Logo"}
+			/>
+			<div className='absolute inset-0 flex flex-col items-center before:absolute before:inset-0 before:bg-slate-400 before:transition-opacity before:duration-300 before:hover:opacity-0 before:opacity-20 before:rounded-md'>
+				<h2 className='text-white text-2xl font-bold'>{title}</h2>
+				<div className=' flex flex-col justify-end w-full h-full'>
+					<div className='w-full flex flex-col justify-end'>
+						<p className='text-gray-800 text-center leading-tight pb-5 bg-white bg-opacity-50'>
+							<strong>{levels}</strong>
+						</p>
+						<div className='w-full bg-gray-400 h-2.5 rounded-b-md'>
+							<div
+								className={`${progressBarPercent} bg-red-700 h-2.5 ${
+									level == 100 ? "rounded-b-md" : "rounded-bl-md"
+								} `}
+							></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
