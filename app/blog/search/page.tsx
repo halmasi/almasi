0411430fb7137e062b3e300e4704624cf8c1636b
Hossein Almasi <h.almasi2012@gmail.com>
2024-09-29 19:47:17 +0330

    add suspense to search page

--- app/blog/search/page.tsx
+++ app/blog/search/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-export default function useSearchPage() {
+function Search() {
   const searchParams = useSearchParams();
   const search = searchParams.get("q");
 
   return <h1 className="text-2xl">{search}</h1>;
+}
+
+export default function useSearchPage() {
+  return (
+    <Suspense>
+      <Search />
+    </Suspense>
+  );
 }
