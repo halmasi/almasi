1b774a1a444392aff1774d4b9336a36b383b130b
Hossein Almasi <h.almasi2012@gmail.com>
2024-10-13 17:38:50 +0330

    initial db in lib and change names

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,83 +3,83 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 
 export default function ContactForm() {
-	interface Inputs {
-		name: string;
-		email: string;
-		message: string;
-	}
+  interface Inputs {
+    name: string;
+    email: string;
+    message: string;
+  }
 
-	const { register, handleSubmit, reset } = useForm<Inputs>();
-	const onSubmit: SubmitHandler<Inputs> = async (data) => {
-		async function sendMessage() {
-			try {
-				const response = await fetch("/api/v1/messages", {
-					method: "POST",
-					body: JSON.stringify(data),
-				});
-				if (response) {
-					const result = await response.json();
-					return result.message;
-				}
-			} catch (e) {
-				console.log(e);
-			} finally {
-				reset();
-			}
-		}
-		alert(
-			"Your message has been send,\nmessages text:\n" + (await sendMessage()),
-		);
-	};
+  const { register, handleSubmit, reset } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    async function sendMessage() {
+      try {
+        const response = await fetch("/api/v1/messages", {
+          method: "POST",
+          body: JSON.stringify(data),
+        });
+        if (response) {
+          const result = await response.json();
+          return result.message;
+        }
+      } catch (e) {
+        console.log(e);
+      } finally {
+        reset();
+      }
+    }
+    alert(
+      "Your message has been send,\nmessages text:\n" + (await sendMessage())
+    );
+  };
 
-	return (
-		<form
-			className='flex flex-col space-y-2 w-full items-start border p-3 rounded-2xl'
-			onSubmit={handleSubmit(onSubmit)}
-		>
-			<fieldset className='flex flex-col md:flex-row w-full'>
-				<label htmlFor='name' className='w-fit pr-3'>
-					Name:
-				</label>
-				<input
-					className='px-4 h-auto w-full bg-white rounded-full border transition-all duration-200 hover:border-red-700 shadow-md focus:shadow-lg focus:outline-none focus:ring-2 ring-red-900'
-					type='text'
-					id='name'
-					{...register("name", { required: true })}
-					required
-					placeholder='Name'
-				/>
-			</fieldset>
-			<fieldset className='flex flex-col md:flex-row w-full'>
-				<label htmlFor='email' className='w-full  md:w-fit pr-3'>
-					Email:
-				</label>
-				<input
-					className='px-4 h-auto w-full bg-white rounded-full border transition-all duration-200 hover:border-red-700 shadow-md focus:shadow-lg focus:outline-none focus:ring-2 ring-red-900'
-					type='email'
-					id='email'
-					{...register("email", { required: true })}
-					required
-					placeholder='Email'
-				/>
-			</fieldset>
-			<label htmlFor='message' className='w-full pr-3'>
-				Massage:
-			</label>
-			<textarea
-				className='px-4 h-[25svh] w-full bg-white rounded-md border transition-all duration-200 hover:border-red-700 shadow-md focus:shadow-lg focus:outline-none focus:ring-2 ring-red-900'
-				{...register("message", { required: true })}
-				id='message'
-				required
-				placeholder='Message'
-			/>
-			<button
-				type='submit'
-				name='submit'
-				className='bg-green-700 text-white rounded-lg py-2 px-5 hover:bg-green-800 self-center'
-			>
-				Submit
-			</button>
-		</form>
-	);
+  return (
+    <form
+      className="flex flex-col space-y-2 w-full items-start border p-3 rounded-2xl"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <fieldset className="flex flex-col md:flex-row w-full">
+        <label htmlFor="name" className="w-fit pr-3">
+          Name:
+        </label>
+        <input
+          className="px-4 h-auto w-full bg-white rounded-full border transition-all duration-200 hover:border-red-700 shadow-md focus:shadow-lg focus:outline-none focus:ring-2 ring-red-900"
+          type="text"
+          id="name"
+          {...register("name", { required: true })}
+          required
+          placeholder="Name"
+        />
+      </fieldset>
+      <fieldset className="flex flex-col md:flex-row w-full">
+        <label htmlFor="email" className="w-full  md:w-fit pr-3">
+          Email:
+        </label>
+        <input
+          className="px-4 h-auto w-full bg-white rounded-full border transition-all duration-200 hover:border-red-700 shadow-md focus:shadow-lg focus:outline-none focus:ring-2 ring-red-900"
+          type="email"
+          id="email"
+          {...register("email", { required: true })}
+          required
+          placeholder="Email"
+        />
+      </fieldset>
+      <label htmlFor="message" className="w-full pr-3">
+        Massage:
+      </label>
+      <textarea
+        className="px-4 h-[25svh] w-full bg-white rounded-md border transition-all duration-200 hover:border-red-700 shadow-md focus:shadow-lg focus:outline-none focus:ring-2 ring-red-900"
+        {...register("message", { required: true })}
+        id="message"
+        required
+        placeholder="Message"
+      />
+      <button
+        type="submit"
+        name="submit"
+        className="bg-green-700 text-white rounded-lg py-2 px-5 hover:bg-green-800 self-center"
+      >
+        Submit
+      </button>
+    </form>
+  );
 }
--- components/homePage/SkillCard.tsx
+++ components/homePage/SkillCard.tsx
@@ -1,45 +1,45 @@
 import Image from "next/image";
-import { levelCalc, iconNameGenerator } from "@/lib/utils";
+import { levelCalc, iconNameGenerator } from "@/lib/skills";
 
 interface Props {
-	title: string;
-	level: number;
+  title: string;
+  level: number;
 }
 
 export default function Skill({ title, level }: Props) {
-	const { levels, progressBarPercent } = levelCalc(level);
+  const { levels, progressBarPercent } = levelCalc(level);
 
-	return (
-		<div className='flex flex-col w-full space-y-3 max-w-64'>
-			<h2 className='text-black text-center pt-3 pb-1 rounded-lg lg:text-xl font-bold'>
-				{title}
-			</h2>
-			<div className='relative w-full'>
-				<div className='overflow-hidden flex flex-row flex-grow rounded-full w-full aspect-square justify-center bg-white  items-center'>
-					<Image
-						className=' w-full object-cover rounded-full'
-						fill
-						quality={10}
-						src={`/icons/${iconNameGenerator(title)}`}
-						alt={title + "Logo"}
-					/>
+  return (
+    <div className="flex flex-col w-full space-y-3 max-w-64">
+      <h2 className="text-black text-center pt-3 pb-1 rounded-lg lg:text-xl font-bold">
+        {title}
+      </h2>
+      <div className="relative w-full">
+        <div className="overflow-hidden flex flex-row flex-grow rounded-full w-full aspect-square justify-center bg-white  items-center">
+          <Image
+            className=" w-full object-cover rounded-full"
+            fill
+            quality={10}
+            src={`/icons/${iconNameGenerator(title)}`}
+            alt={title + "Logo"}
+          />
 
-					<div className='absolute rounded-full overflow-hidden inset-0 flex flex-col items-center before:absolute before:inset-0 before:bg-slate-400 before:transition-opacity before:duration-300 before:hover:opacity-0 before:opacity-20 before:rounded-full'>
-						<div className='flex flex-col w-full bg-white/85 items-center justify-center' />
-						<div className=' flex flex-col justify-end w-full h-full'>
-							<div className='w-full flex flex-col justify-end'>
-								<p className='text-gray-900 text-xs text-center leading-tight pb-5 bg-white bg-opacity-75'>
-									<strong>{levels}</strong>
-								</p>
-							</div>
-						</div>
-					</div>
+          <div className="absolute rounded-full overflow-hidden inset-0 flex flex-col items-center before:absolute before:inset-0 before:bg-slate-400 before:transition-opacity before:duration-300 before:hover:opacity-0 before:opacity-20 before:rounded-full">
+            <div className="flex flex-col w-full bg-white/85 items-center justify-center" />
+            <div className=" flex flex-col justify-end w-full h-full">
+              <div className="w-full flex flex-col justify-end">
+                <p className="text-gray-900 text-xs text-center leading-tight pb-5 bg-white bg-opacity-75">
+                  <strong>{levels}</strong>
+                </p>
+              </div>
+            </div>
+          </div>
 
-					<div
-						className={`absolute w-[105%] bg-gradient-conic from-red-800 via-red-600 to-gray-300 from-[1%] ${progressBarPercent} aspect-square -z-10 rounded-full `} //
-					/>
-				</div>
-			</div>
-		</div>
-	);
+          <div
+            className={`absolute w-[105%] bg-gradient-conic from-red-800 via-red-600 to-gray-300 from-[1%] ${progressBarPercent} aspect-square -z-10 rounded-full `} //
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
