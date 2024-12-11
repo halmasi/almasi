04ab6a9a10d7886a3ce2b4384a7ec3b75f600c0c
halmasi <h.almasi2012@gmail.com>
2024-12-11 15:05:28 +0330

    fix logged in check

--- app/api/v1/login/route.ts
+++ app/api/v1/login/route.ts
@@ -5,38 +5,62 @@ import * as jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 
 export async function GET() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("token");
-  let state = token;
-  return NextResponse.json({ state });
+	const cookieStore = cookies();
+	const token = cookieStore.get("token");
+	let result = {
+		message: { message: "Unauthorized" },
+		status: { status: 401 },
+		cookies: "",
+	};
+	if (token && token.value) {
+		jwt.verify(token.value, process.env.SECRET_KEY!, (e, decode) => {
+			if (decode && typeof decode == "object" && decode.email && decode.name) {
+				const refreshToken = jwt.sign(
+					{ email: decode.email, name: decode },
+					process.env.SECRET_KEY!,
+					{ expiresIn: "1h" },
+				);
+				result.message = {
+					message: "new cookie has been set, you are now logged in",
+				};
+				result.status = { status: 200 };
+				result.cookies = refreshToken;
+			} else if (e) {
+				console.error(e);
+			}
+		});
+	}
+	const response = NextResponse.json(result.message, result.status);
+	response.cookies.set("token", result.cookies);
+	return response;
 }
 
 export async function POST(request: Request) {
-  const data = await request.json();
+	const data = await request.json();
 
-  const email = data.email;
-  const password = data.password;
+	const email = data.email;
+	const password = data.password;
 
-  const admin = await db.admin.findFirst({ where: { email } });
-  if (!admin || !verifyPassword(admin?.password, password))
-    return NextResponse.json({ error: "user not found!" }, { status: 404 });
-  const response = NextResponse.json({
-    message: "new cookie has been set, you are now logged in",
-  });
-  const token = jwt.sign(
-    { email: admin.email, name: admin.name },
-    process.env.SECRET_KEY!,
-    { expiresIn: "1h" }
-  );
-  response.cookies.set("token", JSON.stringify(token));
+	const admin = await db.admin.findFirst({ where: { email } });
+	if (!admin || !verifyPassword(admin?.password, password))
+		return NextResponse.json({ error: "user not found!" }, { status: 404 });
+	const response = NextResponse.json({
+		message: "new cookie has been set, you are now logged in",
+	});
+	const token = jwt.sign(
+		{ email: admin.email, name: admin.name },
+		process.env.SECRET_KEY!,
+		{ expiresIn: "1h" },
+	);
+	response.cookies.set("token", token);
 
-  return response;
+	return response;
 }
 
 export async function DELETE() {
-  const response = NextResponse.json({
-    messageid: `cookies has been deleted, you are now logged out`,
-  });
-  response.cookies.delete("token");
-  return response;
+	const response = NextResponse.json({
+		messageid: `cookies has been deleted, you are now logged out`,
+	});
+	response.cookies.delete("token");
+	return response;
 }
