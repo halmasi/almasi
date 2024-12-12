ac156e36e80f2122f81f2528121e2a9b2452d647
halmasi <h.almasi2012@gmail.com>
2024-12-13 00:48:50 +0330

    implement single message operations

--- app/api/v1/messages/[messageId]/route.ts
+++ app/api/v1/messages/[messageId]/route.ts
@@ -1,17 +1,25 @@
+import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request, context: any) {
 	const { params } = context;
 	const data = params.messageId;
-
-	//get message by id
-	return NextResponse.json({ messageid: data });
+	const message = await db.message.findFirst({
+		where: { id: data },
+	});
+	return NextResponse.json({
+		id: message?.id,
+		name: message?.name,
+		email: message?.email,
+		createdAt: message?.createdAt,
+		content: message?.content,
+	});
 }
 
 export async function DELETE(request: Request, context: any) {
 	const { params } = context;
 	const data = params.messageId;
-
+	await db.message.delete({ where: { id: data } });
 	//delete message by id
 	return NextResponse.json({ messageid: `deleted message id: ${data}` });
 }
